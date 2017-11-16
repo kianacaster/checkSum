21e75bfa471e66a08fd7129008a7b398dd621706
Kian Acaster <acasterkian@gmail.com>
2017-11-16 19:57:58 +0000

    Reducing the code

--- checkSum.cpp
+++ checkSum.cpp
@@ -7,9 +7,7 @@ bool checkSum(int* array, int desiredSum)
     for(int i = 0; i < sizeof(array) - 1; i++){
         if(i == 0) differences.push_back(desiredSum - array[i]);
         for(int j = 0; j < sizeof(differences) - 1; j++){
-            if((desiredSum - array[i]) == differences[j]){
-                return true;
-            }
+            if((desiredSum - array[i]) == differences[j]) return true;
         }
     }
     return false;
